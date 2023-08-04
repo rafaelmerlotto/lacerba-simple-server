82433b7e3a607e6f0f53fcb6603955f299ea8942
root <root@LAPTOP-NFTP3RDA>
2023-08-04 18:21:39 +0200

    url parameters

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
     return res.status(200).send("<h1>Hello Rafael</h1> <p> Questo è il nostro primo server</p>");
 })
 
+app.get('/greeting/:name', (req, res) => {
+    console.log(req.params)
+    const name: string = req.params.name;
+    return res.send(`<h1>Hello ${name}</h1> <p> Questa e la pagina greeting</p>`);
+})
+
 app.get('/Ciao', (req, res) => {
     return res.send("<h1>Ciao Rafael</h1> <p> Questa è una pagina Ciao</p>");
 })
